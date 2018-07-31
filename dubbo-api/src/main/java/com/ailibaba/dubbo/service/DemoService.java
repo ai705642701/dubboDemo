e84c382e9d09d35a645b03951faa96ea29eb33ee
caochengwu <ai705642701@163.com>
2018-07-31 11:26:21 +0800

    Initial commit

---
+++ dubbo-api/src/main/java/com/ailibaba/dubbo/service/DemoService.java
@@ -0,0 +1,7 @@
+package com.ailibaba.dubbo.service;
+
+import java.util.List;
+
+public interface DemoService {
+    List<String> getPermissions(Long id);
+}
---
+++ dubbo-provider/src/main/java/com/ailibaba/dubbo/demo/web/Provider.java
@@ -0,0 +1,15 @@
+package com.ailibaba.dubbo.demo.web;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Provider {
+
+    public static void main(String[] args) throws Exception {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("application-context.xml");
+        System.out.println(context.getDisplayName() + ": here");
+        context.start();
+        System.out.println("服务已经启动...");
+        System.in.read();
+    }
+
+}
